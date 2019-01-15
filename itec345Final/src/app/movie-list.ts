eb8cb9f0657205a45efe46e2e0254411ad9ab64c
Aleix Culture <you@example.com>
2019-01-15 06:30:47 -0400

    addedcritique and movie list files

---
+++ itec345Final/src/app/movie-list.ts
@@ -0,0 +1,213 @@
+import {
+    Movie
+  } from './movies';:
+  
+  export const MOVIES: Movie[] = [{
+      Name: 'Black Pather',
+      Genre: 'Fantasy',
+      ReleaseDate: '29/01/18',
+      Rating: 'pg-13',
+      UserScore: '6.6',
+      Metascore: '88',
+      Length: '134',
+      Description: 'After the events of Captain America: Civil War, King TíChalla returns home to the reclusive, technologically advanced African nation of Wakanda to serve as his countryís new leader. However, TíChalla soon finds that he is challenged for the throne from factions within his own country. When two foes conspire to destroy Wakanda, the hero known as Black Panther must team up with C.I.A. agent Everett K. Ross and members of the Dora Milaje, Wakanadan special forces, to prevent Wakanda from being dragged into a world war'
+    },
+    {
+      Name: 'Avengers Infinity War',
+      Genre: 'Action',
+      ReleaseDate: '27/04/18',
+      Rating: 'pg-13',
+      UserScore: '8.6',
+      Metascore: '68',
+      Length: '149',
+      Description: 'As the Avengers and their allies have continued to protect the world from threats too large for any one hero to handle, a new danger has emerged from the cosmic shadows: Thanos. A despot of intergalactic infamy, his goal is to collect all six Infinity Stones, artifacts of unimaginable power, and use them to inflict his twisted will on all of reality. Everything the Avengers have fought for has led up to this moment ñ the fate of Earth and existence itself has never been more uncertain'
+    },
+    {
+      Name: 'Venom',
+      Genre: 'Action',
+      ReleaseDate: '05/10/18',
+      Rating: 'pg-13',
+      UserScore: '6.3',
+      Metascore: '35',
+      Length: '112',
+      Description: 'One of Marvel'
+      s most enigmatic,
+      complex and badass characters comes to the big screen,
+      starring Academy Award nominated actor Tom Hardy as the lethal protector Venom '
+    },
+    {
+      Name: 'The Amazing Spiderman 2',
+      Genre: 'Action',
+      ReleaseDate: '02/05/14',
+      Rating: 'pg-13',
+      UserScore: '6.5',
+      Metascore: '53',
+      Length: '142',
+      Description: 'Itís great to be Spider-Man. For Peter Parker, thereís no feeling quite like swinging between skyscrapers, embracing being the hero, and spending time with Gwen. But being Spider-Man comes at a price: only Spider-Man can protect his fellow New Yorkers from the formidable villains that threaten the city. With the emergence of Electro, Peter must confront a foe far more powerful than he. And as his old friend, Harry Osborn, returns, Peter comes to realize that all of his enemies have one thing in common: Oscorp'
+    },
+    {
+      Name: 'Bright',
+      Genre: 'Action',
+      ReleaseDate: '22/12/17',
+      Rating: 'tv-ma',
+      UserScore: '7.2',
+      Metascore: '29',
+      Length: '117',
+      Description: 'Set in an alternate present-day where humans, orcs, elves, and fairies have been co-existing since the beginning of time. Bright is genre-bending action movie that follows two cops from very different backgrounds. Ward (Will Smith) and Jakoby (Joel Edgerton), embark on a routine patrol night and encounter a darkness that will ultimately alter the future and their world as they know it'
+  
+    },
+    {
+      Name: 'The Huntsman: Winter\'s War',
+      Genre: 'Action',
+      ReleaseDate: '22/04/16',
+      Rating: 'pg-13',
+      UserScore: '5.2',
+      Metascore: '35',
+      Length: '114',
+      Description: 'Long before the evil Queen Ravenna (Charlize Theron) was thought vanquished by Snow Whiteís blade, she watched silently as her sister, Freya (Emily Blunt), suffered a heartbreaking betrayal and fled their kingdom. With Freyaís ability to freeze any enemy, the young ice queen has spent decades in a remote wintry palace raising a legion of deadly huntsmenóincluding Eric (Chris Hemsworth) and warrior Sara (Jessica Chastain)óonly to find that her prized two defied her one demand: Forever harden your hearts to love. When Freya learns of her sisterís demise, she summons her remaining soldiers to bring the Magic Mirror home to the only sorceress left who can harness its power. But once she discovers Ravenna can be resurrected from its golden depths, the wicked sisters threaten this enchanted land with twice the darkest force itís ever seen. Now, their amassing army shall prove undefeatableÖunless the banished huntsmen who broke their queenís cardinal rule can fight their way back to one another'
+    },
+    {
+      Name: 'Hansel and Gretel: Witch Hunters',
+      Genre: 'Action',
+      ReleaseDate: '25/01/13',
+      Rating: 'r',
+      UserScore: '5.7',
+      Metascore: '23',
+      Length: '88',
+      Description: 'Catching up with Hansel and Gretel 15 years after their incident involving a gingerbread house, the siblings have evolved into bounty hunters who hunt witches'
+    },
+    {
+      Name: 'The Last Witch Hunter',
+      Genre: 'Action',
+      ReleaseDate: '23/10/15',
+      Rating: 'pg-13',
+      UserScore: '6.5',
+      Metascore: '34',
+      Length: '106',
+      Description: 'The modern world holds many secrets, but the most astounding secret of all is that witches still live amongst us; vicious supernatural creatures intent on unleashing the Black Death upon the world. Armies of witch hunters battled the unnatural enemy across the globe for centuries, including Kaulder (Vin Diesel), a valiant warrior who managed to slay the all-powerful Queen Witch, decimating her followers in the process. In the moments right before her death, the Queen curses Kaulder with her own immortality, forever separating him from his beloved wife and daughter in the afterlife. Today Kaulder is the only one of his kind remaining, and has spent centuries hunting down rogue witches, all the while yearning for his long-lost loved ones. However, unbeknownst to Kaulder, the Queen Witch is resurrected and seeks revenge on her killer causing an epic battle that will determine the survival of the human race'
+    },
+    {
+      Name: '300 Rise of an Empire',
+      Genre: 'Action',
+      ReleaseDate: '07/03/14',
+      Rating: 'r',
+      UserScore: '6.0',
+      Metascore: '48',
+      Length: '102',
+      Description: 'The Greek general Themistocles battles an invading army of Persians led by mortal-turned-god Xerxes, and Artemisia, commander of the Persian navy'
+    },
+    {
+      Name: 'Maleficient',
+      Genre: 'Action',
+      ReleaseDate: '30/05/14',
+      Rating: 'pg',
+      UserScore: '6.8',
+      Metascore: '56',
+      Length: '97',
+      Description: 'Driven by revenge and a fierce desire to protect the moors over which she presides, Maleficent cruelly places an irrevocable curse upon the human kingís newborn infant Aurora. As the child grows, Aurora is caught in the middle of the seething conflict between the forest kingdom she has grown to love and the human kingdom that holds her legacy. Maleficent realizes that Aurora may hold the key to peace in the land and is forced to take drastic actions that will change both worlds forever'
+    },
+    {
+  
+      Name: 'Frozen',
+      Genre: 'Action',
+      ReleaseDate: '27/11/13',
+      Rating: 'pg',
+      UserScore: '7.5',
+      Metascore: '74',
+      Length: '102',
+      Description: 'Anna sets off on an epic journeyóteaming up with rugged mountain man Kristoff and his loyal reindeer Svenóto find her sister Elsa, whose icy powers have trapped the kingdom of Arendelle in eternal winter. Encountering Everest-like conditions, mystical trolls and a hilarious snowman named Olaf, Anna and Kristoff battle the elements in a race to save the kingdom'
+    },
+    {
+      Name: 'Moana',
+      Genre: 'Action',
+      ReleaseDate: '23/11/16',
+      Rating: 'pg',
+      UserScore: '7.8',
+      Metascore: '81',
+      Length: '107',
+      Description: 'Moana, a spirited teenager and born navigator, sets sail from the ancient South Pacific islands of Oceania in search of a fabled island. During her incredible journey, she teams up with her hero, the legendary demi-god Maui, to traverse the open ocean on an action-packed voyage, encountering enormous sea creatures, breathtaking underworlds and ancient folklore'
+    },
+    {
+      Name: 'Inside Out',
+      Genre: 'Adventure',
+      ReleaseDate: '19/06/15',
+      Rating: 'pg',
+      UserScore: '8.7',
+      Metascore: '94',
+      Length: '94',
+      Description: 'Growing up can be a bumpy road, and itís no exception for Riley, who is uprooted from her Midwest life when her father starts a new job in San Francisco. Like all of us, Riley is guided by her emotions: Joy (Amy Poehler), Fear (Bill Hader), Anger (Lewis Black), Disgust (Mindy Kaling) and Sadness (Phyllis Smith). The emotions live in Headquarters, the control center inside Rileyís mind, where they help advise her through everyday life. As Riley and her emotions struggle to adjust to a new life in San Francisco, turmoil ensues in Headquarters. Although Joy, Rileyís main and most important emotion, tries to keep things positive, the emotions conflict on how best to navigate a new city, house, and school'
+    },
+    {
+      Name: 'Open Season',
+      Genre: 'Adventure',
+      ReleaseDate: '29/12/06',
+      Rating: 'pg',
+      UserScore: '6.1',
+      Metascore: '49',
+      Length: '83',
+      Description: 'A domesticated grizzly bear finds that there'
+      s more to life than being the star attraction of a mountain town nature show when a fast - talking mule deer offers him a crash course in woodland living'
+    },
+    {
+      Name: 'Rush Hour 3',
+      Genre: 'Action, Thriller, Comedy, Crime',
+      ReleaseDate: '10/08/07',
+      Rating: 'pg-13',
+      UserScore: '6.4',
+      Metascore: '44',
+      Length: '91',
+      Description: 'LAPD Detective James Carter and Chinese Chief Inspector Lee respectively must travel to Paris to battle a wing of the Chinese organized crime family, the Triads';
+    },
+    {
+      Name: 'Ice Age: Collision Course',
+      Genre; : 'Action',
+      ReleaseDate; : '22/07/16',
+      Rating; : 'g',
+      UserScore; : '4.1',
+      Metascore; : '34',
+      Length; : '94',
+      Description; : 'Scratís epic pursuit of the elusive acorn catapults him into the universe where he accidentally sets off a series of cosmic events that transform and threaten the Ice Age World. To save themselves, Sid, Manny, Diego, and the rest of the herd must leave their home and embark on a quest full of comedy and adventure, traveling to exotic new lands and encountering a host of colorful new characters';
+    },
+    {
+      Name: 'Storks',
+      Genre; : 'Adventure',
+      ReleaseDate; : '23/09/16',
+      Rating; : 'pg',
+      UserScore; : '6.3',
+      Metascore; : '56',
+      Length; : '87',
+      Description; : 'Storks deliver babiesÖor at least they used to. Now they deliver packages for global internet giant Cornerstore.com. Junior, the companyís top delivery stork, is about to be promoted when he accidentally activates the Baby Making Machine, producing an adorable and wholly unauthorized baby girl. Desperate to deliver this bundle of trouble before the boss gets wise, Junior and his friend Tulip, the only human on Stork Mountain, race to make their first-ever baby drop ñ in a wild and revealing journey that could make more than one family whole and restore the storksí true mission in the world';
+    },
+    {
+      Name: 'Home',
+      Genre; : 'Adventure',
+      ReleaseDate; : '27/03/15',
+      Rating; : 'pg',
+      UserScore; : '7.1',
+      Metascore; : '55',
+      Length; : '94',
+      Description; : 'When Earth is taken over by the overly-confident Boov, an alien race in search of a new place to call home, all humans are promptly relocated. But when one resourceful girl, Tip (Rihanna), manages to avoid capture, she finds herself the accidental accomplice of a banished Boov named Oh (Jim Parsons). The two fugitives realize thereís a lot more at stake than intergalactic relations as they embark on the road trip of a lifetime';
+    },
+    {
+      Name: 'Bolt',
+      Genre; : 'Adventure',
+      ReleaseDate; : '21/11/08',
+      Rating; : 'pg',
+      UserScore; : '8.4',
+      Metascore; : '67',
+      Length; : 96; '';
+      Description: 'More than just manís best friend, Bolt is a modern day action hero. Raised on the set of a hit television show, Bolt believes he has amazing powers ñ like a devastating Superbark! But when heís accidentally shipped to New York City, Boltís daredevil stunts no longer go according to plan. As reality begins to challenge his delusions, Bolt learns his whole life has been a lie. Or has it? Amidst the adversity and hard-knocks, Bolt will discover that a dogís true superpower lies in his loyalty to the owner he loves';
+    },
+    {
+      Name: 'Suicide Squad',
+      Genre; : 'Action',
+      ReleaseDate; : '05/08/16',
+      Rating; : 'pg-13',
+      UserScore; : '6.1',
+      Metascore; : '40',
+      Length; : '123',
+      Description; : 'It feels good to be badÖ Assemble a team of the worldís most dangerous, incarcerated Super-Villains, provide them with the most powerful arsenal at the governmentís disposal, and send them off on a mission to defeat an enigmatic, insuperable entity. U.S. intelligence officer Amanda Waller has determined only a secretly convened group of disparate, despicable individuals with next to nothing to lose will do. However, once they realize they werenít picked to succeed but chosen for their patent culpability when they inevitably fail, will the Suicide Squad resolve to die trying, or decide itís every man for himself?';
+    }
+  
+  ]
+  
