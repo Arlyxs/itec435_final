453ab6f3a6d0754462999d724b39672a0c0c5c46
Aleix Culture <you@example.com>
2019-01-17 04:58:19 -0400

    changed length from decimal to mins

--- itec345Final/src/app/movies/movies.component.ts
+++ itec345Final/src/app/movies/movies.component.ts
@@ -15,7 +15,7 @@ export class MoviesComponent implements OnInit {
     rating: 'pg-13',
     userScore: 6.6,
     metaScore: 88,
-    length: 1.34,
+    length: 134,
     // tslint:disable-next-line:max-line-length
     description: 'After the events of Captain America: Civil War, King TíChalla returns home to the reclusive, technologically advanced African nation of Wakanda to serve as his countryís new leader. However, TíChalla soon finds that he is challenged for the throne from factions within his own country. When two foes conspire to destroy Wakanda, the hero known as Black Panther must team up with C.I.A. agent Everett K. Ross and members of the Dora Milaje, Wakanadan special forces, to prevent Wakanda from being dragged into a world war',
   } ;
